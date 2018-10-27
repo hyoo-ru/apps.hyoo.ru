0ae88e62053f3cac888b79ef840b6e086e9933ba
nin-jin <nin-jin@ya.ru>
2018-10-27 13:02:27 +0300

    Compat with non https apps.

--- showcase.view.ts
+++ showcase.view.ts
@@ -1,22 +1,10 @@
 namespace $.$$ {
 
-	export class $hyoo_showcase extends $.$hyoo_showcase {
-
-		content() {
-			return Object.keys( this.apps() ).map( uri => this.App( uri ) )
-		}
-
-		app_title( uri : string ) {
-			return this.apps()[ uri ]
-		}
-
-		app_uri( uri : string ) {
-			return uri
-		}
-
-	}
-
 	export class $hyoo_showcase_app extends $.$hyoo_showcase_app {
+
+		background() {
+			return this.preview() ? `url(${ JSON.stringify( this.preview() ) })` : ''
+		}
 
 	}
 
