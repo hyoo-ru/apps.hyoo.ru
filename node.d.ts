46358c14d29e5b580fc6163aebfd4fd5494d3213
nin-jin <nin-jin@users.noreply.github.com>
2022-05-19 07:11:08 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1498,7 +1498,6 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_check_expand extends $mol_check {
-        minimal_height(): number;
         Icon(): $mol_icon_chevron;
         level(): number;
         style(): {
