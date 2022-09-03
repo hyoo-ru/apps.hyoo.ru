ccec42d4f468369c7b80ae5b14fbc1e0cb62fa25
nin-jin <nin-jin@users.noreply.github.com>
2022-09-03 07:09:59 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -4004,7 +4004,7 @@ var $;
             return {
                 ...super.attr(),
                 href: this.uri_toggle(),
-                title: this.hint(),
+                title: this.hint_safe(),
                 target: this.target(),
                 download: this.file_name(),
                 mol_link_current: this.current()
@@ -4029,6 +4029,9 @@ var $;
         }
         hint() {
             return "";
+        }
+        hint_safe() {
+            return this.hint();
         }
         target() {
             return "_self";
@@ -4157,6 +4160,15 @@ var $;
             target() {
                 return this.external() ? '_blank' : '_self';
             }
+            hint_safe() {
+                try {
+                    return this.hint();
+                }
+                catch (error) {
+                    $mol_fail_log(error);
+                    return '';
+                }
+            }
         }
         __decorate([
             $mol_mem
@@ -4281,7 +4293,7 @@ var $;
                 disabled: this.disabled(),
                 role: "button",
                 tabindex: this.tab_index(),
-                title: this.hint()
+                title: this.hint_safe()
             };
         }
         sub() {
@@ -4317,6 +4329,9 @@ var $;
         }
         hint() {
             return "";
+        }
+        hint_safe() {
+            return this.hint();
         }
         error() {
             return "";
@@ -4501,6 +4516,15 @@ var $;
                     return $mol_fail_hidden(error);
                 }
                 return String(error.message ?? error);
+            }
+            hint_safe() {
+                try {
+                    return this.hint();
+                }
+                catch (error) {
+                    $mol_fail_log(error);
+                    return '';
+                }
             }
             sub_visible() {
                 return [
@@ -6200,7 +6224,7 @@ var $;
             scheduled = false;
             await $mol_test_run();
             $$.$mol_test_complete();
-        }, 0);
+        }, 1000);
     }
     $_1.$mol_test_schedule = $mol_test_schedule;
     $_1.$mol_test_mocks.push(context => {
