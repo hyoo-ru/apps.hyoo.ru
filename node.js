aeadc0a310d4404f29bd2234ac01fa6dff21f915
nin-jin <nin-jin@users.noreply.github.com>
2023-12-13 07:08:52 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -7602,8 +7602,8 @@ var $;
             return [];
         }
         Body_content() {
-            const obj = new this.$.$mol_list();
-            obj.rows = () => this.body();
+            const obj = new this.$.$mol_view();
+            obj.sub = () => this.body();
             return obj;
         }
         body_content() {
@@ -7728,6 +7728,9 @@ var $;
             },
             Body_content: {
                 padding: $mol_gap.block,
+                flex: {
+                    direction: 'column',
+                },
                 justify: {
                     self: 'stretch',
                 },
