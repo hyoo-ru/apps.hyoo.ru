9a9c7f8e93452eec04bea35f334bf29a2d306d1a
nin-jin <nin-jin@users.noreply.github.com>
2026-03-24 07:52:20 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -7517,6 +7517,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
